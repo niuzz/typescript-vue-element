48655a1d94dc650d9334ba2facfa75748d1b0ae0
niuzz <niuzz@hotmail.com>
2019-11-29 12:29:13 +0800

    feat: axios

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,17 @@
+import { Message } from "element-ui";
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import service from "./utils/https";
 
 Vue.config.productionTip = false;
+
+Vue.prototype.$https = service;
+Vue.prototype.$message = Message;
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: h => h(App),
 }).$mount("#app");
---
+++ src/utils/https.ts
@@ -0,0 +1,63 @@
+import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from "axios";
+import { Message } from "element-ui";
+
+// 响应接口
+export interface ResponseData {
+  code: number;
+  data?: any;
+  message: string;
+}
+
+let service: AxiosInstance | any = null;
+
+if (process.env.NODE_ENV === "development") {
+  service = axios.create({
+    baseURL: "/api",
+    timeout: 50000,
+  });
+} else {
+  service = axios.create({
+    baseURL: "/api",
+    timeout: 50000,
+  });
+}
+
+// request 拦截器 axios 的一些配置
+service.interceptors.request.use(
+  (config: AxiosRequestConfig) => {
+    return config;
+  },
+  (error: any) => {
+    // Do something with request error
+    console.error("error:", error); // for debug
+    Promise.reject(error);
+  },
+);
+
+// respone 拦截器 axios 的一些配置
+service.interceptors.response.use(
+  (res: AxiosResponse) => {
+    // Some example codes here:
+    // code == 0: success
+    if (res.status === 200) {
+      const data: ResponseData = res.data;
+      if (data.code === 0) {
+        return data.data;
+      } else {
+        Message({
+          message: data.message,
+          type: "error",
+        });
+      }
+    } else {
+      Message({
+        message: "网络错误!",
+        type: "error",
+      });
+      return Promise.reject(new Error(res.data.message || "Error"));
+    }
+  },
+  (error: any) => Promise.reject(error),
+);
+
+export default service;
